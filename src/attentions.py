fe60cd48abc91e0b5d0d8bf5e7e02bf127bd21ff
whsqkaak <whsqkaak@naver.com>
2022-08-01 14:14:31 +0900

    Create MultiHead Attention and Update all attention mechanism to batch version.

--- src/attentions.py
+++ src/attentions.py
@@ -17,7 +17,6 @@ class DotProductAttention(nn.Module):
     3. Compute attenion value(context vector) by weighted sum of attention weights and values.
     4. Return attention value and attention weights.
     """
-    # TODO: To update batch version.
 
     def __init__(self):
         super().__init__()
@@ -38,18 +37,26 @@ class DotProductAttention(nn.Module):
             A tuple of attention value and attention weights about query and value.
 
         Shape:
-            query: :math:`(N_t, E)` where N_t is query length(target sequence length),
+            query: :math:`(B, N_t, E)` where 
+                B is batch size,
+                N_t is query length(target sequence length),
                 E is embedding dimension.
-            value: :math:`(N_s, E)` where N_s is value length(source sequence length),
+            value: :math:`(B, N_s, E)` where
+                B is batch size,
+                N_s is value length(source sequence length),
                 E is embedding dimension.
             
             Returns:
-                attention value: :math:`(N_t, E)`
-                attention weights: :math: `(N_t, N_s)`
+                attention value: :math:`(B, N_t, E)`
+                attention weights: :math: `(B, N_t, N_s)`
         """
-        attn_score = torch.mm(query, value.transpose(0, 1))
+        # (B, N_t, E) x (B, E, N_s) -> (B, N_t, N_s)
+        attn_score = torch.bmm(query, value.transpose(-2, -1))
+        
         attn_weights = F.softmax(attn_score, dim=-1)
-        attn_value = torch.mm(attn_weights, value)
+        
+        # (B, N_t, N_s) x (B, N_s, E) -> (B, N_t, E)
+        attn_value = torch.bmm(attn_weights, value)
         return attn_value, attn_weights
 
 
@@ -63,7 +70,7 @@ class GeneralAttention(nn.Module):
 
     def __init__(self, embed_dim: int):
         super().__init__()
-        self.query_proj = nn.Linear(embed_dim, embed_dim, bias=False)
+        self.query_proj = nn.Linear(embed_dim, embed_dim)
 
     def forward(
         self,
@@ -81,26 +88,34 @@ class GeneralAttention(nn.Module):
             A tuple of attention value and attention weights about query and value.
 
         Shape:
-            query: :math:`(N_t, E)` where N_t is the target sequence length,
-                and E is embedding dimension.
-            value: :math:`(N_s, E)` where N_s is the source sequence length,
-                and E is embedding dimension.
+            query: :math:`(B, N_t, E)` where 
+                B is batch size,
+                N_t is the target sequence length,
+                E is embedding dimension.
+            value: :math:`(B, N_s, E)` where 
+                B is batch size,
+                N_s is the source sequence length,
+                E is embedding dimension.
 
             Returns:
-                attention value: :math:`(N_t, E)`
-                attention weights: :math:`(N_t, N_s)`
+                attention value: :math:`(B, N_t, E)`
+                attention weights: :math:`(B, N_t, N_s)`
         """
         query = self.query_proj(query)
-        attn_score = torch.mm(query, value.transpose(0, 1))
+        
+        # (B, N_t, E) x (B, E, N_s) -> (B, N_t, N_s)
+        attn_score = torch.bmm(query, value.transpose(-2, -1))
         attn_weights = F.softmax(attn_score, dim=-1)
-        attn_value = torch.mm(attn_weights, value)
+        
+        # (B, N_t, N_s) x (B, N_s, E) -> (B, N_t, E)
+        attn_value = torch.bmm(attn_weights, value)
         return attn_value, attn_weights
 
 
 class ScaledDotProductAttention(nn.Module):
     """
     This class is implementation of Scaled Dot-Product Attention mechanism
-    proposed in "Attention is All you Need"(Vaswani et al., 2017).
+    proposed in "Attention Is All You Need"(Vaswani et al., 2017).
 
     Computes scaled dot product attntion on query, key and value tensors.
     """
@@ -131,31 +146,196 @@ class ScaledDotProductAttention(nn.Module):
             A tuple of attention value and attention weights.
 
         Shape:
-            query: :math:`(N_t, E)` where N_t is the target sequence length,
-                and E is embedding dimension.
-            key: :math:`(N_s, E)` where N_s is the source sequence length,
-                and E is embedding dimension.
-            value: :math:`(N_s, E)` where N_s is the source sequence length,
-                and E is embedding dimension.
-            attn_mask: :math:`(N_t, N_s)` where N_t is the target sequence length,
-                and N_s is the source sequence length.
+            query: :math:`(B, N_t, E)` where 
+                B is batch size,
+                N_t is the target sequence length,
+                E is embedding dimension.
+            key: :math:`(B, N_s, E)` where 
+                B is batch size,
+                N_s is the source sequence length,
+                E is embedding dimension.
+            value: :math:`(B, N_s, E)` where 
+                B is batch size,
+                N_s is the source sequence length,
+                E is embedding dimension.
+            attn_mask: either a 3D tensor of shape :math:`(B, N_t, N_s)` or a 2D tensor of
+                shape :math:`(N_t,N_s)` where
+                B is batch size,
+                N_t is the target sequence length,
+                N_s is the source sequence length.
 
             Returns:
-                attention value: :math:`(N_t, E)`
-                attention weights: :math:`(N_t, N_s)`
+                attention value: :math:`(B, N_t, E)`
+                attention weights: :math:`(B, N_t, N_s)`
         """
-        embed_dim = query.shape[1]
+        embed_dim = query.shape[-1]
 
-        attn_score = torch.mm(query, key.transpose(0, 1))
+        # (B, N_t, E) x (B, E, N_s) -> (B, N_t, N_s)
+        attn_score = torch.bmm(query, key.transpose(-2, -1))
         attn_score = attn_score / math.sqrt(embed_dim)
         
         if attn_mask is not None:
-            attn_score.masked_fill(mask, -float('Inf'))
+            attn_score = torch.baddbmm(attn_mask, query, key.transpose(-2, -1))
+        else:
+            attn_score = torch.bmm(query, key.transpose(-2, -1))
+            
+        attn_score = attn_score / math.sqrt(embed_dim)
 
         attn_weights = F.softmax(attn_score, dim=-1)
-        attn_value = torch.mm(attn_weights, value)
+        
+        # (B, N_t, N_s) x (B, N_s, E) -> (B, N_t, E)
+        attn_value = torch.bmm(attn_weights, value)
         
         return attn_value, attn_weights
 
 
+class MultiHeadAttention(nn.Module):
+    """
+    This class is implementation of Multi-Head Attention
+    proposed in "Attention Is All You Need"(Vaswani et al., 2017).
+    
+    Allows the modlr to jointly attend to information
+    from different representation subspaces.
+    
+    """
 
+    def __init__(
+        self,
+        embed_dim: int,
+        num_heads: int
+    ):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+        
+        assert self.head_dim * num_heads == self.embed_dim, "embed_dim must be divisible by num_heads"
+        
+        self.query_proj = nn.Linear(embed_dim, embed_dim)
+        self.key_proj = nn.Linear(embed_dim, embed_dim)
+        self.value_proj = nn.Linear(embed_dim, embed_dim)
+        self.out_proj = nn.Linear(embed_dim, embed_dim)
+        
+    def forward(
+        self,
+        query: Tensor,
+        key: Tensor,
+        value: Tensor,
+        attn_mask: Optional[Tensor] = None
+    ) -> Tuple[Tensor, Tensor]:
+        """
+        Args:
+            query:
+                Query Embedding tensors.
+            key:
+                Key Embedding tensors.
+            value:
+                Value Embedding tensors.
+            attn_mask:
+                Optional tensor containing mask values to be added to calculated
+                attention.
+
+        Returns:
+            A tuple of attention value and attention weights about query, key and value.
+
+        Shape:
+            query: :math:`(B, N_t, E)` where 
+                B is batch size,
+                N_t is the target sequence length,
+                E is embedding dimension.
+            key: :math:`(B, N_s, E)` where 
+                B is batch size,
+                N_s is the source sequence length,
+                E is embedding dimension.
+            value: :math:`(B, N_s, E)` where 
+                B is batch size,
+                N_s is the source sequence length,
+                E is embedding dimension.
+            attn_mask: either a 3D tensor of shape :math:`(B, N_t, N_s)` or a 2D tensor of
+                shape :math:`(N_t,N_s)` where
+                B is batch size,
+                N_t is the target sequence length,
+                N_s is the source sequence length.
+
+            Returns:
+                attention value: :math:`(B, N_t, E)`
+                attention weights: :math:`(B, h, N_t, N_s)` where
+                    h is a number of attention heads.
+        """
+        batch_size, target_len, _ = query.shape
+        source_len = key.shape[1]
+        
+        # Compute Linear Projection
+        query = self.query_proj(query)
+        key = self.key_proj(key)
+        value = self.value_proj(value)
+        
+        # Reshape query, key, value for multihead attention
+        query = self._reshape_for_multihead_attn(query)
+        key = self._reshape_for_multihead_attn(key)
+        value = self._reshape_for_multihead_attn(value)
+
+        # Reshape attention mask
+        if attn_mask is not None:
+            attn_mask = attn_mask.repeat(self.head_num, 1, 1)
+            
+        attn_value, attn_weights = ScaledDotProductAttention()(query, key, value, attn_mask)
+        
+        # Reshape attention value
+        # `(B * h, N_t, E // h)` -> `(N_t, B * h, E // h)`
+        attn_value = attn_value.transpose(0, 1).contiguous()
+        
+        # `(N_t, B * h, E // h)` -> (N_t, B, E)`
+        attn_value = attn_value.view(target_len, batch_size, self.embed_dim)
+        
+        # `(N_t, B, E)` -> `(B, N_t, E)`
+        attn_value = attn_value.transpose(0, 1)
+        
+        # Compute out projection(W^O)
+        attn_value = self.out_proj(attn_value)
+        
+        # Reshape attention weights
+        #`(B * h, N_t, N_s)` -> `(B, h, N_t, N_s)`
+        attn_weights = attn_weights.view(batch_size, self.num_heads, target_len, source_len)
+        
+        return attn_value, attn_weights
+        
+        
+    def _reshape_for_multihead_attn(
+        self,
+        emb_tensor: Tensor
+    ) -> Tensor:
+        """
+        Reshape emb_tensor(query, key or value) for multihead attention.
+        
+        Args:
+            emb_tensor:
+                The embedding tensor.
+                Query, Key or Value Tensor.
+            
+        Returns:
+            Reshaped emb_tensor for multihead attention.
+            
+        Shape:
+            emb_tensor: :math`(B, N, E)` where
+                B is batch size,
+                N is sequence(target or source) length,
+                E is embedding dimension.
+            
+            Returns: :math`(B * h, N, E // h)` where
+                B is batch size,
+                h is a number of attention heads,
+                N is sequence(target or source) length,
+        """
+        batch_size, seq_len, _ = emb_tensor.shape
+        
+        # `(B, N, E)` -> `(N, B, E)`
+        reshaped_tensor = emb_tensor.transpose(0, 1).contiguous()
+        
+        # `(N, B, E)` -> `(N, B * h, E // h)`
+        reshaped_tensor = reshaped_tensor.view(seq_len, batch_size * self.num_heads, self.head_dim)
+        
+        # `(N, B * h, E // h)` -> `(B * h, N, E // h)`
+        reshaped_tensor = reshaped_tensor.transpose(0, 1)
+        
+        return reshaped_tensor
--- test/test_attentions.py
+++ test/test_attentions.py
@@ -9,7 +9,8 @@ import torch
 from src.attentions import (
     DotProductAttention,
     GeneralAttention,
-    ScaledDotProductAttention
+    ScaledDotProductAttention,
+    MultiHeadAttention
     )
 
 def attention_test():
@@ -17,17 +18,21 @@ def attention_test():
     print(f"Prepare dummy data.")
     
     # Make dummy data.
-    N_t = 2
-    N_s = 3
-    E = 4
-    query = torch.randn(N_t, E)
-    key = torch.randn(N_s, E)
-    value = torch.randn(N_s, E)
+    B = 3
+    N_t = 16
+    N_s = 20
+    E = 384
+    num_heads = 8
+    attn_mask = None
+    query = torch.randn(B, N_t, E)
+    key = torch.randn(B, N_s, E)
+    value = torch.randn(B, N_s, E)
 
-    print(f"Start Dot-Product Attention test.")
-    attentions = ["dot", "general", "scaled"]
+    print(f"Start Attention test.")
+    attentions = ["dot", "general", "scaled", "multihead"]
 
     for type_attention in attentions:
+        # Compute attention value and weights.
         if type_attention == "dot":
             attn = DotProductAttention()
             attn_value, attn_weights = attn(query, value)
@@ -38,16 +43,25 @@ def attention_test():
 
         elif type_attention == "scaled":
             attn = ScaledDotProductAttention()
-            attn_value, attn_weights = attn(query, key, value, attn_mask=None)
+            attn_value, attn_weights = attn(query, key, value, attn_mask)
+        
+        elif type_attention == "multihead":
+            attn = MultiHeadAttention(E, num_heads)
+            attn_value, attn_weights = attn(query, key, value, attn_mask)
 
-        print(f"Attention Value: {attn_value}")
-        print(f"Attention Weights: {attn_weights}")
-
-        assert attn_value.shape == (N_t, E)
-        assert attn_weights.shape == (N_t, N_s)
+        # Check attention value and weights.
+        assert attn_value.shape == (B, N_t, E)
+        assert attn_weights.shape == (B, N_t, N_s) or attn_weights.shape == (B, num_heads, N_t, N_s)
     
-        for i in range(N_t):
-            assert round(float(attn_weights[i].sum()), 0) == 1.0
+        if type_attention == "multihead":
+            for i in range(B):
+                for j in range(num_heads):
+                    for k in range(N_t):
+                        assert round(float(attn_weights[i][j][k].sum()), 0) == 1.0
+        else:
+            for i in range(B):
+                for j in range(N_t):
+                    assert round(float(attn_weights[i][j].sum()), 0) == 1.0
     
 
 def test_main():
